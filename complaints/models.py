fcfd2baf4d160c1e27428e636ee74dbcf9d202e3
Malde Moksh <maldemoksh437@gmail.com>
2026-01-24 08:52:10 +0530

    Add Complaint model with status tracking and unique constraint per student

--- complaints/models.py
+++ complaints/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from students.models import Student
 # Create your models here.
+
+class Complaint(models.Model):
+    ATTENTION_LEVEL = [
+        ("I", "Immediate"),
+        ("H", "High"),
+        ("M", "Medium")
+    ]
+    COMPLAINT_STATUS = [
+        ("O", "Opened"),
+        ("R", "Resolved"),
+        ("U", "UnOpened"),
+    ]
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.CharField()
+    status = models.CharField(max_length=1, choices=COMPLAINT_STATUS)
+    attention_level = models.CharField(max_length=1, choices=ATTENTION_LEVEL)    
+    def __str__(self):
+        return f"{self.student.user.full_name} -> {self.status} -> {self.attention_level}"
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["student"],
+                name="unique_complaints_per_student"
+            )
+        ]
