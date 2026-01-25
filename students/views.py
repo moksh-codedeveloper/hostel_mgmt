6b2704ebb91ecbd872b55d26aeaff33b38f89864
Malde Moksh <maldemoksh437@gmail.com>
2026-01-25 09:23:23 +0530

    Implement user registration and login views with JSON responses

--- students/views.py
+++ students/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from .models import User, Student
+import json
 # Create your views here.
+
+
+def register(request):
+    if request.method != "POST":
+        return JsonResponse({"message" : "not supported METHOD"}, status=405)
+    data = json.load(request.body)
+    user = User.objects.create_user(
+        username=data["username"],
+        password=data["password"],
+        full_name=data["full_name"]
+    )
+    login(request=request, user=user)
+    return JsonResponse({
+        "next" : "/students/register/"
+    })
+
+def user_login(request):
+    if request.method != "POST":
+        return JsonResponse({"message" : "not Supported METHOD"}, status=405)
+    user = json.load(request.body)
+    authenticated_user = authenticate(
+        request=request,
+        username= user["username"],
+        password= user["password"]
+    )
+    if not authenticated_user:
+        return JsonResponse({"message" : "Invalid Credentials"}, status=403)
+    login(request, authenticated_user)
+    return JsonResponse({"message" : "Login successful", "next" : "/dashboard"})
