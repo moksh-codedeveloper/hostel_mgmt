a9127016711021739d3ff9dd986f1c7095c4caf1
Malde Moksh <maldemoksh437@gmail.com>
2026-01-28 08:09:52 +0530

    Add update student URL path to urlpatterns

--- students/urls.py
+++ students/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import register, student_profile, user_login, register_user, user_logout
+from .views import register, student_profile, user_login, register_user, user_logout, update_student
 
 urlpatterns = [
-    path("register/", register),
-    path("login/", user_login),
-    path("student/create/", register_user),
-    path("student/", student_profile),
-    path("logout/", user_logout)
+    path("register/", register, name="register"),
+    path("login/", user_login, name="login"),
+    path("student/create/", register_user, name="user_register"),
+    path("student/", student_profile, name="student_profile"),
+    path("logout/", user_logout, name="user_logout"),
+    path('update/', update_student, name="update_student")
 ]
