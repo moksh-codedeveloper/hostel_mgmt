5434ba8f178a2d2d97544f63b5b0c4eba17bac62
Malde Moksh <maldemoksh437@gmail.com>
2026-01-29 16:02:47 +0530

    Update delete complaint URL to include complaint ID as a parameter

--- complaints/urls.py
+++ complaints/urls.py
@@ -3,6 +3,6 @@ from .views import create_complain, delete_complaint, list_complaints, update_st
 urlpatterns = [
     path("create/", create_complain, name="create_complaints"),
     path("update/", update_status,name="update_status"),
-    path("delete/", delete_complaint,name="delete_complaint"),
+    path("delete/<complaint_id>/", delete_complaint,name="delete_complaint"),
     path("list/", list_complaints, name="list_complaints")
 ]
